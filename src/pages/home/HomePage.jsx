589a3b88e1e8610692b43f1e879cff44708b0aa2
AbdulboisNigmatjonov17 <abdulboisnigmatjonov@gmail.com>
2025-02-11 00:33:05 +0500

    button qoshildi qoganini chorshanba qsam kere

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 import BannerForm from '../../components/banner/BannerForm'
 import FooterMain from '../../components/footer/FooterMain'
 import MainCards from '../../components/cards/MainCards'
-// import Comment from '../../components/comments/Comment'
 
 const HomePage = () => {
   return (
     <>
       <BannerForm />
-      <MainCards/>
-      <FooterMain/>
-      {/* <Comment/> */}
+      <MainCards />
+      <div className='w-full flex justify-center items-center my-5 '>
+        <button className='border border-[#00C29F] py-2.5 p-7 rounded-md text-[#00C29F] cursor-pointer'>Show more</button>
+      </div>
+      <FooterMain />
     </>
   )
 }
